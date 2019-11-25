a093a07ed8bdb64dfa074f203f92260d5343b3d4
HernanFranco96 <hernan.franco051096@gmail.com>
2019-11-25 12:27:57 -0300

    Listo

--- 4 - PHP y MySQL/Clase 2/Ejercicio_3_While_Alumni.php
+++ 4 - PHP y MySQL/Clase 2/Ejercicio_3_While_Alumni.php
@@ -8,6 +8,7 @@
     <style>
         table{border:1px solid #000;margin:auto;}
         .caja1{width:100px;background-color: violet; height:100px;border:1px solid #000;}
+        .caja2{width:100px;background-color: green; height:100px;border:1px solid #000;}
     </style>
 </head>
 <body>
@@ -28,7 +29,10 @@
                 while($cantColumnas < $columna)
                 {
                     $cantColumnas++;
-                    $html = $html . "<td class='caja1'></td>"; 
+                    if($cantFilas%2==0)
+                        $html = $html . "<td class='caja1'></td>"; 
+                    if($cantFilas%2!=0)
+                        $html = $html . "<td class='caja2'></td>"; 
                 }    
                 $html = $html . "</tr>";
             }
