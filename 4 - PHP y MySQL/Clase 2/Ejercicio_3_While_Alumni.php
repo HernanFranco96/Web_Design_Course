5395da65362d85bcf01b8a1d3aaeebc1d8a32717
HernanFranco96 <hernan.franco051096@gmail.com>
2019-11-25 11:56:13 -0300

    Ultimo ejercicio de la clase 2 terminado

---
+++ 4 - PHP y MySQL/Clase 2/Ejercicio_3_While_Alumni.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio 3</title>
+    <style>
+        table{border:1px solid #000;margin:auto;}
+        .caja1{width:100px;background-color: violet; height:100px;border:1px solid #000;}
+    </style>
+</head>
+<body>
+    <table>
+        <?php
+            $cantFilas = 0;
+            $filas = 6;
+            $columna = 10;
+
+            $html = "";
+
+            while($cantFilas < $filas)
+            {
+                $cantFilas++;
+                $cantColumnas = 0;
+
+                $html = $html . "<tr class='caja1'>";
+                while($cantColumnas < $columna)
+                {
+                    $cantColumnas++;
+                    $html = $html . "<td class='caja1'></td>"; 
+                }    
+                $html = $html . "</tr>";
+            }
+
+            echo $html;
+        ?>
+    </table>
+</body>
+</html>
