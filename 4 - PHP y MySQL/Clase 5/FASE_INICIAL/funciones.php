b7fde4c8d0d1aa043f869c62413ee6421b24a103
HernanFranco96 <hernan.franco051096@gmail.com>
2019-12-06 16:09:41 -0300

    Arreglando error en funciones.php

--- 4 - PHP y MySQL/Clase 5/FASE_INICIAL/funciones.php
+++ 4 - PHP y MySQL/Clase 5/FASE_INICIAL/funciones.php
@@ -6,57 +6,31 @@
     function validarFormulario($nombre, $email, $msj)
     {
         $codMsj = "";
-        $index = true;
         // Validacion del nombre
 
-        while($index != false)
-        {
-            if(isset($nombre) && is_string($nombre) && strlen($nombre) >= 3 && ctype_alpha($nombre) == true)
-            {
-                $codMsj = "A0C000";
-            }
             if(ctype_alnum($nombre) && ctype_alpha($nombre) == false) //Si es alfanumerico pero si no es solamente alfabetico.
             {
                 $codMsj = "A0C001";
-                $index = false;
             }
-            if(ctype_digit($nombre))
+            elseif(ctype_digit($nombre))
             {
                 $codMsj = "A0C002";
-                $index = false;
             }
-            if(strlen($nombre) < 3)
+            elseif(strlen($nombre) < 3)
             {
                 $codMsj = "A0C003";
-                $index = false;
             }
-
-            // Validacion del email
-
-            if(isset($email) && is_string($email) && strlen($email) >= 16 && strlen($email) <= 40 && filter_var($email, FILTER_VALIDATE_EMAIL))
-            {
-                $codMsj = "B0C000";
-            }
-            if(strlen($email) < 16)
+            elseif(strlen($email) < 16) // Validacion del email
             {
                 $codMsj = "B0C001";
-                $index = false;
             }
-            
-            
-            // Validacion del mensaje
-            if(isset($msj) && is_string($msj) && strlen($msj) > 0 && ctype_alnum($msj) == true)
-            {
-                $codMsj = "C0C000";
-            }
-            if(strlen($msj) >= 300)
+            elseif(strlen($msj) >= 300) // Validacion del mensaje
             {
                 $codMsj = "C0C001";
-                $index = false;
-            }
-
-            $index = false;
-
+            }       
+            else{
+                $codMsj = "A0C000";
+            }     
             /*
             $destinatario = "hernan.franco051096@gmail.com";
             $asunto = "Contacto Web";
@@ -81,7 +55,7 @@
                 $codMsj = "D0C001";
             }
             */
-        }
+        
 
         return $codMsj;
     }
@@ -93,16 +67,7 @@
         {
             
             case 'A0C000':  
-                $mensaje = "NOMBRE CORRECTO";
-                break;
-            case 'B0C000':
-                $mensaje = "EMAIL CORRECTO";
-                break;
-            case 'C0C000': 
-                $mensaje = "MENSAJE CORRECTO";
-                break;
-            case 'D0C000': 
-                $mensaje = "EMAIL ENVIADO";
+                $mensaje = "FORMULARIO COMPLETADO CORRECTAMENTE ";
                 break;
             case 'A0C001':
                 $mensaje = "NOMBRE INCORRECTO: No puede contener numeros y letras.";
