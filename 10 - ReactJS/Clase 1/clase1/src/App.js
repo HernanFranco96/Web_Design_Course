72f8b7e271d254342cbaaba8c6f869b57bbb678e
HernanFranco96 <hernan.franco051096@gmail.com>
2020-09-17 12:57:20 -0300

    Primer clase de React. Ejecutamos el primer archivo

---
+++ 10 - ReactJS/Clase 1/clase1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Cabecera from './Cabecera.js';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecera titulo="Las props en react!!"></Cabecera>
+    </div>
+  );
+}
+
+export default App;
---
+++ 10 - ReactJS/Clase 1/clase1/src/Cabecera.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cabecera(props){
+   return (<header><h1>{props.titulo}</h1></header>)
+}
+
+export default Cabecera;
